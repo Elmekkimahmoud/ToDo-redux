b0112133f2d278e456aeb1b2f9f2707a5769efe2
Elmekkimahmoud <59558915+Elmekkimahmoud@users.noreply.github.com>
2020-02-24 10:04:50 +0100

    Updates

---
+++ precache-manifest.4ee44ade337fc765e87ad66d54611db2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c33d716228c08e9f58ad1ce6ed343b1",
+    "url": "/ToDo-redux/index.html"
+  },
+  {
+    "revision": "385839a2b71573112bec",
+    "url": "/ToDo-redux/static/css/main.dfd02019.chunk.css"
+  },
+  {
+    "revision": "7cf439b319a8987e7746",
+    "url": "/ToDo-redux/static/js/2.4fb1cbf4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ToDo-redux/static/js/2.4fb1cbf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385839a2b71573112bec",
+    "url": "/ToDo-redux/static/js/main.ead42844.chunk.js"
+  },
+  {
+    "revision": "45aa81165ec62d4d3190",
+    "url": "/ToDo-redux/static/js/runtime-main.1b8165cc.js"
+  }
+]);
